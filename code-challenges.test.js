a8d5428254c0c8df555a77e04fcde08bf28b6d5e
AlejaH1213 <alejajara2@gmail.com>
2023-10-01 16:21:54 -0700

    assessment done

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,9 +1,7 @@
 // ASSESSMENT 5: JavaScript Coding Practical Questions with Jest
-
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
-
 // Use test driven development to complete the following questions
 // Add appropriate dependencies: $ yarn add jest
 
@@ -13,46 +11,114 @@
 // --------------------1) Create a function that takes in a string and returns a string with a coded message. The coded message converts "a" to 4, "e" to 3, "i" to 1, and "o" to 0.
 
 // a) Create a test with expect statements using the variables provided.
+describe("codeFunc", () => {
+    it("takes in a string and returns a string with a coded message.", () => {
+        const secretCodeWord1 = "Lackadaisical"
+        // Expected output: "L4ck4d41s1c4l"
+        const secretCodeWord2 = "Gobbledygook"
+        // Expected output: "G0bbl3dyg00k"
+        const secretCodeWord3 = "Eccentric"
+        // Expected output: "3cc3ntr1c"
+    expect(codeFunc(secretCodeWord1)).toEqual("L4ck4d41s1c4l")
+    expect(codeFunc(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+    expect(codeFunc(secretCodeWord3)).toEqual("3cc3ntr1c")
 
-const secretCodeWord1 = "Lackadaisical"
-// Expected output: "L4ck4d41s1c4l"
-const secretCodeWord2 = "Gobbledygook"
-// Expected output: "G0bbl3dyg00k"
-const secretCodeWord3 = "Eccentric"
-// Expected output: "3cc3ntr1c"
-
+    })
+})
+// Good Failure: ReferenceError: codeFunc is not defined
 // b) Create the function that makes the test pass.
 
 // Pseudo code:
-
+// name: codeFunc
+// input: a string
+// output: a string with a coded message
+// process: 
+// first im going to create a function and give it a string as parameter
+// im going to use .replaceAll and inside Ill make sure to account for capitalized and lower case words
+// first im going to use the replace all for a inside a variable, then ill create the variable for e and reference the variable for e so I make sure that im changing all the vowels in the string and transforming the string one vowel at the time and then Ill return the last of the variables in this case codedWordO
+const codeFunc = (string) => {
+    let codedWordA = string.replaceAll(/a|A/g, 4)
+    let codedWordE = codedWordA.replaceAll(/e|E/g, 3)
+    let codedWordI = codedWordE.replaceAll(/i|I/g, 1)
+    let codedWordO = codedWordI.replaceAll(/o|O/g, 0)
+    return codedWordO
+}
+// TEST PASSED
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
 
 // a) Create a test with expects statement using the variable provided.
-
-const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
-
-const filterLetterA = "a"
-// Expected output: ["Mango", "Apricot", "Peach"]
-const filterLetterE = "e"
-// Expected output: ["Cherry", "Blueberry", "Peach"]
-
+describe("arrayContainsLetter", () => {
+    it("takes in an array of words and a single letter and returns an array of all the words containing that particular letter.", () => {
+        const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+        
+        const filterLetterA = "a"
+        // Expected output: ["Mango", "Apricot", "Peach"]
+        const filterLetterE = "e"
+        // Expected output: ["Cherry", "Blueberry", "Peach"]
+        expect(arrayContainsLetter(fruitArray, filterLetterA)).toEqual(["Mango", "Apricot", "Peach"])
+        expect(arrayContainsLetter(fruitArray, filterLetterE)).toEqual(["Cherry", "Blueberry", "Peach"])
+    } )
+})
+// Good Failure: ReferenceError: arrayContainsLetter is not defined
 // b) Create the function that makes the test pass.
 
 // Pseudo code:
+// name: arrayContainsLetter
+// input: an array and a single letter
+// output: returns an array of all the words containing that particular letter
+// process:
+// first im going to create the function and give it two parameters, one of array and the other of letter
+// then im going to add the higher order function of .filter to the array, and then ill make sure that I include .toLowerCase() both to the value and to the letter to ensure that the function will work on capitalized words and then Ill use the include method to check if the value inside the array contains the letter and return the array with the values that contain the letter
 
+// i think it is with filter and look at each value and if the value is the same as the letter then to keep it in the array?
+
+const arrayContainsLetter = (array, letter) => {
+    return array.filter((value) => value.toLowerCase().includes(letter.toLowerCase()))
+}
+// TEST PASSED
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
 
 // a) Create a test with expect statements using the variable provided.
-
-const hand1 = [5, 5, 5, 3, 3]
-// Expected output: true
-const hand2 = [5, 5, 3, 3, 4]
-// Expected output: false
-const hand3 = [5, 5, 5, 5, 4]
-// Expected output: false
-const hand4 = [7, 2, 7, 2, 7]
-// Expected output: true
-
+describe("fullHouse", () => {
+    it("takes in an array of 5 numbers and determines whether or not the array is a full house.", () => {
+        const hand1 = [5, 5, 5, 3, 3]
+        // Expected output: true
+        const hand2 = [5, 5, 3, 3, 4]
+        // Expected output: false
+        const hand3 = [5, 5, 5, 5, 4]
+        // Expected output: false
+        const hand4 = [7, 2, 7, 2, 7]
+        // Expected output: true
+    expect(fullHouse(hand1)).toEqual(true)
+    expect(fullHouse(hand2)).toEqual(false)
+    expect(fullHouse(hand3)).toEqual(false)
+    expect(fullHouse(hand4)).toEqual(true)
+    })
+})
+// Good failure: ReferenceError: fullHouse is not defined
 // b) Create the function that makes the test pass.
 
 // Pseudo code:
+// name: fullHouse
+// input: an array of 5 numbers
+// output: a true or false statement
+// process:
+// im going to create a function that has a parameter of array
+// then im going to create two variables, one for the three of the kind, and one for the double.
+// inside each variable im going to use a find method and inside of it a filter method to check if the numbers are the same, and then ill put the lenght to also make sure that there are three numbers that are the same or two numbers that are the same
+// if the array has three values that are the same or two values that are the same then they are going to be inside these two variables
+//then im going to put an if statement that checks if the two variables that i created have values inside and if they do then it will return true and if they dont it will return false
+
+
+const fullHouse = (array) => {
+    const threeOfAKind = array.find((value) => array.filter((number) => number === value).length === 3)
+    const double = array.find((value) => array.filter((number) => number === value).length === 2)
+
+    if (threeOfAKind && double){
+    return true
+    } else {
+        return false
+    }
+}
+// TEST PASSED
+
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -10,7 +10,21 @@ filter_letter_o = 'o'
 filter_letter_t = 't'
 # Expected output: ['tea', 'water', 'soda water']
 
-# Pseudo code:
+
+# name: single_word
+# input: an array and a single letter
+# output: an array with words that contain that letter
+# process: 
+# im going to create a function called single_word and im going to give it two parameters one of the array and the other one of the letter
+# then im going to use the select method to filter the array according to the letter, so it is going to look in each word of the array for the words that have the letter and return them in a new array 
+def single_word (array, letter)
+    array.select do |word|
+        word.include?(letter)
+    end 
+end
+
+p single_word(beverages_array, filter_letter_o)
+p single_word(beverages_array, filter_letter_t)
 
 # -------------------2) Create a method that takes in a hash and returns one array with all the hash values at their own index and in alphabetical order. No nested arrays. Use the test variable provided.
 # HINT: Google 'ruby get rid of nested arrays'
@@ -19,18 +33,78 @@ us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['Califor
 # Expected output: ['Arizona', 'California', 'Idaho', 'Maine', 'Nevada', 'New Hampshire', 'Oregon', 'Rhode Island', 'Washington'] 
 
 # Pseudo code:
+# name: alphabetical_array
+# input: a hash
+# output: an array with all the values in alphabetical order
+# process:
+# first i need to be able to access the values inside the hash so im going to use .values to return  all the values
+# then im going to flatten the array so its only one array
+# finally im going to sort the array so it is in alphabetical order
+def alphabetical_array hash
+   values = hash.values
+   flat_array = values.flatten
+   sorted_array = flat_array.sort
+end
 
+p alphabetical_array us_states
 # --------------------3a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a bike_info method that returns a sentence with all the data from the bike object.
 
 # Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.'
 
 # Pseudo code:
+# name:
+# process:
+# im going to create a class called bike
+# ill use the initialize method and inside im only putting model because it is the only parameter that needs an argument, inside the method im going to put the model the wheels and the current speed according to the instructions
+# then im going to create the bike info method with the string interpolation using model, wheels and current speed
+# then im going to instantiate my class and give it the argument of 'Trek'
+# finally im going to call the method 
+class Bike 
+    attr_accessor :model, :wheels, :current_speed
+def initialize(model)
+        @model= model
+        @wheels= 2
+        @current_speed= 0
+    end
+    def bike_info 
+        "The #{@model} bike has #{@wheels} wheels and is going #{@current_speed} mph"
+    end
 
 # -------------------3b) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed by a given amount. The brake method should decrease the speed by a given amount. The bike cannot go negative speeds.
-
+    
 # Expected output example: my_bike.pedal_faster(10) => 10
 # Expected output example: my_bike.pedal_faster(18) => 28
 # Expected output example: my_bike.brake(5) => 23
 # Expected output example: my_bike.brake(25) => 0
-
 # Pseudo code:
+# first im going to add the attr_accessor to my class so the current speed can be updated 
+# first im going to create the pedal faster method and give it a value as a parameter
+# then im going to tell it to set the current speed plus or equal to the value
+# then im going to create the brake method 
+# im going to tell it to set the update the current speed minus or equal to the value
+# then im going to add the [].max that is going to make sure that the value doesnt go below zero
+    def pedal_faster value
+        @current_speed += value 
+    end
+    def brake value
+        @current_speed -=value 
+        # so this is creating an array with two elements, the current speed and the number zero
+        # then the max method looks for the maximum value in the array, if the current speed is greater than zero then it is going to return the current speed but if the current speed is zero or below zero it is only going to return a zero
+        [@current_speed, 0].max
+    end
+end
+
+new_bike = Bike.new('Trek')
+p new_bike.pedal_faster(10)
+# output: 10
+p new_bike.pedal_faster(18)
+# output: 28
+p new_bike.brake(5)
+# output: 23
+p new_bike.brake(25)
+# output: 0
+
+
+
+
+
